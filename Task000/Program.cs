4285769f382e4fba045a7ae23ef46f3318671dad
[Osman] <developer.zver@mail.ru>
2023-07-21 19:26:27 +0300

    Task000-007

---
+++ Task000/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+System.Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int result = number * number;
+System.Console.WriteLine($"Решение: {number}*{number}");
+System.Console.WriteLine($"Ответ: {result}");
---
+++ Task007/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+Console.WriteLine("Введите трехзначное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0)
+{
+    number *= -1;
+}
+
+if (number >= 100 && number <= 999)
+{
+    int result = number % 10;
+    Console.WriteLine($"Последняя цифра числа: {result}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
+
+
+int result2 = number / 100; // Первая цифра трехзначного числа
+int result3 = number / 10 % 10; // Вторая цифра трехзначного числа
+
+
+/* Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0)
+{
+    Console.WriteLine("Число не натуральное");
+}
+else
+{
+    // for(int i = -number; i <= number; i++){
+    //     Console.Write($"{i} ");
+    // }
+
+    int count = -number;
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count++;
+    }
+} */
