66a46327901fb56d02c63ccfcc9cae4d5a7ad162
[Osman] <developer.zver@mail.ru>
2023-07-27 15:35:12 +0300

    task17-20-23

---
+++ Task020/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int x1Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int x2Coordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Round(
+    DistanceTwoPoints(x1Coordinate, y1Coordinate, x2Coordinate, y2Coordinate),
+    2
+);
+Console.WriteLine(result);
+
+double DistanceTwoPoints(int x1, int y1, int x2, int y2)
+{
+    int cat1 = x1 - x2;
+    int cat2 = y1 - y2;
+    double result = Math.Sqrt(cat1 * cat1 + cat2 * cat2);
+    return result;
+}
---
+++ Task023/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+// Решение 1 по проще
+// System.Console.WriteLine($"Введите число N:");
+// int number = int.Parse(Console.ReadLine()!);
+// System.Console.WriteLine($"Таблици квадратов чисел от 1 до {n}:");
+// for (int i = 1; i <= number; i++)
+// {
+//     System.Console.Write($"{Math.Pow(i, 2)} ");
+// }
+
+
+//  Решение 2 по проще
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Некорректные данные");
+    return;
+}
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int square = i * i;
+        Console.WriteLine($"{i}     {square}");
+    }
+}
